d47ae58a36bafcb87c32bbaa6ca131423ca49436
MusumuriSamson <samsonmusumuri@gmail.com>
2023-04-15 18:27:32 -0500

    Functions in train_classifier  process_data ; Done

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,70 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    :param messages_filepath: This variable takes in the filepath of the messages.csv to read.
+    :param categories_filepath: This variable takes in the filepath of the categories.csv to read.
+
+    :return: The function returns the merged datasets.
+    """
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+
+    df = messages.merge(categories, on='id')
+
+    return df
 
 
 def clean_data(df):
-    pass
+    """
+    :param messages_filepath: This variable takes in the dataframe to clean.
+
+
+    :return: The function returns the processed dataframe.
+    """
+
+    # Split the categories dataframe by ';' and expand into columns.
+    categories = df['categories'].str.split(";", expand=True)
+
+    # Rename the columns with the values from the first row
+    row1 = categories.iloc[0]
+
+    # Keep everything except the number at the end
+    column_names = row1.apply(lambda x: x[:-2]).tolist()
+
+    # Rename columns in the dataframe
+    categories.columns = column_names
+
+    # Loop through all the column values and replace with the last number it holds
+    for col in categories:
+        categories[col] = categories[col].str[-1]
+        # Convert the values to astype(int)
+        categories[col] = categories[col].astype(int)
+
+    # Drop the categories column in the dataframe
+    df = df.drop('categories', axis=1)
+
+    # Concat df and categories dataframes
+    df = pd.concat([df, categories], axis=1)
+
+    # Drop duplicates in dataframe
+    df.drop_duplicates(inplace=True)
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """
+    :param df: This variable takes in the dataframe for which you want to create a table in the database.
+    :param database_filename: This variable takes in the filepath to store the database.
+
+    """
+    engine = create_engine('sqlite:///' + database_filename, echo=False)
+
+    df.to_sql('Disaster_data', engine, if_exists='replace', index=False)
 
 
 def main():
@@ -24,18 +78,18 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
+        print('Please provide the filepaths of the messages and categories '
+              'datasets as the first and second argument respectively, as '
+              'well as the filepath of the database to save the cleaned data '
+              'to as the third argument. \n\nExample: python process_data.py '
+              'disaster_messages.csv disaster_categories.csv '
               'DisasterResponse.db')
 
 
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,25 +1,145 @@
 import sys
-from sklearn.model_selection import train_test_split
+import pandas as pd
+import warnings
+import sqlite3
+import re
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.multioutput import ClassifierChain
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import classification_report
+from sklearn.metrics import precision_score, accuracy_score, recall_score, f1_score
+
+import pickle
+warnings.filterwarnings('ignore')
 
 
 def load_data(database_filepath):
-    pass
+    """
+    :param file_path: takes in the filepath of the database to read.
+    :return: the function returns the table as a dataframe.
+
+    """
+
+    conn = sqlite3.connect(database_filepath)
+
+    df = pd.read_sql_query("SELECT * from Disaster_data", con=conn)
+
+    X = df['message']
+    # Y = df['genre']
+    Y = df.drop(['id', 'message', 'original', 'genre'], axis=1)
+    category_names = Y.columns
+
+    conn.close()
+
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    """
+    :param text: takes in a string value and using nltk methods normalizes,tokenizes and lemmatizes it.
+
+
+    :return: list of nlp processed text.
+
+    """
+
+    # Find all urls if any exists in the text and replace it with the word 'url_placeholder'
+    url_format = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+
+    all_urls = re.findall(url_format, text)
+
+    for u in all_urls:
+        text = text.replace(u, 'url_placeholder')
+
+    # Tokenize the text
+    tokenized_text = word_tokenize(text.lower())
+
+    # Lemmatize the text
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+
+    for token in tokenized_text:
+        lemmatized_text = lemmatizer.lemmatize(token).strip()
+        clean_tokens.append(lemmatized_text)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    """
+        With feature extraction methods from scikit learn this function builds a pipeline with a classifier.
+
+    """
+
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(
+            RandomForestClassifier(min_samples_split=2, n_estimators=10)))
+    ])
+
+    return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """
+    :param model: tT.
+    :param X_test: takes in a set of input features.
+    :param Y_test: takes in a set of input labels.
+    :param category_names: takes in a list of values containing column names
+
+    :return:
+
+    """
+
+    y_pred = model.predict(X_test)
+
+    accuracy_scores = []
+    precision_scores = []
+    f1_scores = []
+    all_recall = []
+
+    for i, cat in enumerate(category_names):
+
+        accuracy_scores.append(accuracy_score(
+            Y_test.values[:, i], y_pred[:, i])*100)
+        precision_scores.append(precision_score(
+            Y_test.values[:, i], y_pred[:, i], average='weighted')*100)
+        f1_scores.append(
+            f1_score(Y_test.values[:, i], y_pred[:, i], average='weighted')*100)
+        all_recall.append(recall_score(
+            Y_test.values[:, i], y_pred[:, i], average='weighted')*100)
+
+    all_scores_dict = dict(zip(category_names, zip(
+        accuracy_scores, precision_scores, f1_scores, all_recall)))
+
+    all_scores_df = pd.DataFrame(all_scores_dict).T
+
+    all_scores_df.columns = ['Accuracy', 'Precision', 'F1', 'Recall']
+
+    all_scores_df = all_scores_df.reset_index().rename(
+        columns={'index': 'Feature'})
+
+    return all_scores_df
 
 
 def save_model(model, model_filepath):
-    pass
+    """
+    :param model: our classification estimator
+    :param model_filepath: location of the path to store our model
+
+    :return:
+
+    """
+
+    with open(model_filepath, 'wb') as file:
+        pickle.dump(model, file)
 
 
 def main():
@@ -27,14 +147,15 @@ def main():
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+        X_train, X_test, Y_train, Y_test = train_test_split(
+            X, Y, test_size=0.2)
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
@@ -44,13 +165,11 @@ def main():
         print('Trained model saved!')
 
     else:
-        print('Please provide the filepath of the disaster messages database '\
-              'as the first argument and the filepath of the pickle file to '\
-              'save the model to as the second argument. \n\nExample: python '\
+        print('Please provide the filepath of the disaster messages database '
+              'as the first argument and the filepath of the pickle file to '
+              'save the model to as the second argument. \n\nExample: python '
               'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
 
 
 if __name__ == '__main__':
     main()
-
-
